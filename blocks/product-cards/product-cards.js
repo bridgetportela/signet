5ae92680778af1b7333b6a13b2156915b0d15d5a
timwlee <timothyl@adobe.com>
2025-11-05 13:35:00 -0800

    Add product-picker block

---
+++ blocks/product-cards/product-cards.js
@@ -0,0 +1,39 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default async function decorate(block) {
+  const products = 'https://main--demo-boilerplate--lamontacrook.hlx.page/misc/products.json?sheet=signet-jared';
+
+  const lastDiv = [...block.children].pop();
+  let skus = lastDiv.querySelector('p');
+  skus = skus ? skus.textContent.split(',') : [''];
+  const ul = document.createElement('ul');
+
+  const productsResponse = await fetch(products);
+  if (productsResponse.ok) {
+    const p = await productsResponse.json();
+    console.log(p);
+    p.data.forEach(product => skus.forEach(sku => {
+      if (product.SKU === sku) {
+        const li = document.createElement('li');
+        const div = document.createElement('div');
+        div.className = 'product-card-image';
+        // const optimizedPic = createOptimizedPicture(product['Image {width:80}'], 'DM Image', false, [{ width: '750' }]);
+        div.innerHTML = `<img src="${product['Image {width:80}']}" alt="${product['Product Name']}">`;
+        const p = document.createElement('p');
+        p.textContent = product['Product Name'];
+
+        // product price 
+        const price = document.createElement('div');
+        const priceValue = parseFloat(product["Price"]);
+        price.textContent = isNaN(priceValue) ? "N/A" : `$${priceValue.toFixed(2)}`;
+        p.append(price);
+
+        li.append(div, p);
+        ul.append(li);
+      }
+    }));
+  }
+  const place =lastDiv.querySelector('div');
+  place.append(ul);
+  // block.append(ul);
+}
